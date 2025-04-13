e0d086e446ddd8c809a2208c8084f104a5115958
Nenad-WindowsPC <nenadstojkovic92@gmail.com>
2025-04-13 17:30:20 +0200

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+echo ">>> Prelazak u direktorijum"
 cd /mnt/Main_data/scripts/server_web_fallback || exit 1
+
+echo ">>> Povlačim promene sa Gita"
 git pull origin main || exit 1
+
+echo ">>> Čekam 5 sekundi..."
 sleep 5
+
+echo ">>> Restartujem server_web_fallback.service"
 sudo systemctl restart server_web_fallback
 
-# Loguj sve u telegram.log
-echo ">>> Pokrećem Telegram notifikaciju..." >> telegram.log
-/usr/bin/python3 /mnt/Main_data/scripts/server_web_fallback/telegram_notify.py >> telegram.log 2>&1
-echo ">>> Gotovo ✅" >> telegram.log
+echo ">>> Pokrećem Telegram notifikaciju..."
+/usr/bin/python3 /mnt/Main_data/scripts/server_web_fallback/telegram_notify.py >> /mnt/Main_data/scripts/server_web_fallback/telegram.log 2>&1
+echo ">>> Gotovo ✅" >> /mnt/Main_data/scripts/server_web_fallback/telegram.log
