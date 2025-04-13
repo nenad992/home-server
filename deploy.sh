12bb59d913a74897ae9ec869ca8dd7deb7427888
Nenad-WindowsPC <nenadstojkovic92@gmail.com>
2025-04-13 17:25:12 +0200

    Add .dockerignore file and update deploy script path for telegram_notify.py

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cd /mnt/Main_data/scripts/server_web_fallback || exit 1
 git pull origin main || exit 1
 sleep 5
 sudo systemctl restart server_web_fallback
-python3 telegram_notify.py
+/usr/bin/python3 /mnt/Main_data/scripts/server_web_fallback/telegram_notify.py
